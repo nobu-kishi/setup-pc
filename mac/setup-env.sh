e16d6362b7259fb239f13ecca14b0c8321b231b2
kishi <shore.tf@gmail.com>
2025-07-01 21:38:21 +0900

    feat: java（corretto-21）追加

--- mac/setup-env.sh
+++ mac/setup-env.sh
@@ -5,18 +5,23 @@ cp ../assets/.aws/config $HOME/.aws/config
 cp ../assets/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
+# Corretto 21系の最新バージョン取得
+corretto_version=$(asdf list all java | grep '^corretto-21' | sort -V | tail -n 1)
 asdf plugin add nodejs
 asdf plugin add python
+asdf plugin add java
 asdf plugin add golang
 asdf plugin add terraform
 asdf install nodejs latest
 asdf install python latest
+asdf install java "$corretto_version"
 asdf install golang latest
 asdf install terraform latest
 
 # プロジェクト毎に設定したい場合は、-uオプションなしで実行
 asdf set -u nodejs latest
 asdf set -u python latest
+asdf set -u java "$corretto_version"
 asdf set -u golang latest
 asdf set -u terraform latest
 
